a6e25652286fad7544260f3382dd2d2b650bf2a8
Jan Jędrzejak <43744269+janjedrzejak@users.noreply.github.com>
2018-10-19 12:18:15 +0200

    first version
    done

---
+++ js/toppings.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $(".salami").click(function(){
+        $("#salami").animate({top: '0px'});
+    });
+});
+$(document).ready(function(){
+    $(".ham").click(function(){
+        $("#ham").animate({top: '0px'});
+    });
+});
+$(document).ready(function(){
+    $(".tomatoes").click(function(){
+        $("#tomatoes").animate({top: '0px'});
+    });
+});
+$(document).ready(function(){
+    $(".onion").click(function(){
+        $("#onion").animate({top: '0px'});
+    });
+});
+$(document).ready(function(){
+    $(".pieczarki").click(function(){
+        $("#pieczarki").animate({top: '0px'});
+    });
+});
+$(document).ready(function(){
+    $(".paprica").click(function(){
+        $("#paprica").animate({top: '0px'});
+    });
+});
+$(document).ready(function(){
+    $(".jalapeno").click(function(){
+        $("#jalapeno").animate({top: '0px'});
+    });
+});
+$(document).ready(function(){
+    $(".cheese").click(function(){
+        $("#cheese").animate({top: '0px'});
+    });
+});
+$(document).ready(function(){
+    $(".pizza_box").click(function(){
+        $("#box").animate({top: '-300px'});
+    });
+});
